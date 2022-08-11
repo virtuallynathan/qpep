d088e36020c8d10f10004c507b49df6cecdd8cfd
parvit (Vittorio Parrella) <vittorioparrella+1@gmail.com>
2022-08-11 08:29:25 +0200

    refactored generation of icons with go2array tool

---
+++ qpep-tray/icons/icons.go
@@ -0,0 +1,19 @@
+//go:build windows
+// +build windows
+
+//go:generate go2array -nolist -platform windows -var ConfigIconData -package icons config_icon.ico
+//go:generate go2array -nolist -platform windows -var ExitIconData -package icons exit_icon.ico
+//go:generate go2array -nolist -platform windows -var RefreshIconData -package icons refresh_icon.ico
+//go:generate go2array -nolist -platform windows -var MainIconData -package icons main_icon.ico
+//go:generate go2array -nolist -platform windows -var MainIconWaiting -package icons main_icon_waiting.ico
+//go:generate go2array -nolist -platform windows -var MainIconConnected -package icons main_icon_connected.ico
+//go:generate go2array -nolist -platform linux -var ConfigIconData -package icons config_icon.png
+//go:generate go2array -nolist -platform linux -var ExitIconData -package icons exit_icon.png
+//go:generate go2array -nolist -platform linux -var RefreshIconData -package icons refresh_icon.png
+//go:generate go2array -nolist -platform linux -var MainIconData -package icons main_icon.png
+//go:generate go2array -nolist -platform linux -var MainIconWaiting -package icons main_icon_waiting.png
+//go:generate go2array -nolist -platform linux -var MainIconConnected -package icons main_icon_connected.png
+
+package icons
+
+// No actual code, only for go:generate
